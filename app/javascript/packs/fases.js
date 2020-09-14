b2629dedb3cdeaaaff443afcaf8697c29272f5b3
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2020-09-14 14:47:09 +0200

    Solució de petits errors en fases

--- app/javascript/packs/fases.js
+++ app/javascript/packs/fases.js
@@ -111,6 +111,12 @@ $(document).on('turbolinks:load', function () {
 	  } else if (valor8 === 'Les bombetes LED consumeixen menys energia') {
 			$('.benfet8').show()
 			enviaPuntuacio(8, true)
+        } else if (valor8 === 'Les bombetes Led consumeixen menys energia') {
+            $('.benfet8').show()
+            enviaPuntuacio(8, true)
+        } else if (valor8 === 'Les bombetes LED consumeixen menys energia.') {
+            $('.benfet8').show()
+            enviaPuntuacio(8, true)
 	  } else {
 			$('.malfet8').show().delay(3000).fadeOut()
 			enviaPuntuacio(8, false)
